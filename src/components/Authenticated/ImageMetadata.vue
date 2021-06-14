005564bdc8d0f66eee9c63fa1811095742d21faf
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2021-06-14 10:24:23 -0400

    refactor: dead code removed

--- src/components/Authenticated/ImageMetadata.vue
+++ src/components/Authenticated/ImageMetadata.vue
@@ -2,28 +2,6 @@
 
 	<v-row justify='center' align='center' class='mt-3' no-gutters>
 		
-		<!-- <v-col cols='12' class='ma-0 pa-0' v-if='!piOnline'>
-		
-			<v-row align='center' justify ='center' class='ma-0 pa-0'>
-
-				<v-col cols='auto' class='ma-0 pa-0'>
-
-					<v-row justify='space-between' align='center' no-gutters class='pulse-animation'>
-
-						<v-col cols='auto' class='ma-0 pa-0'>
-							<v-icon dense color='white' class='mr-1' :small='$vuetify.breakpoint.smAndDown'>{{ mdiAlertCircle }}</v-icon>
-						</v-col>
-
-						<v-col cols='auto' class='ma-0 pa-0' >
-							<span class='white--text' >pi offline</span>
-						</v-col>
-						
-					</v-row>
-
-				</v-col>
-		
-			</v-row>
-		</v-col> -->
 		<app-pi-offline v-if='!piOnline' />
 		
 		<app-display-rows :toDisplay='piInfo' />

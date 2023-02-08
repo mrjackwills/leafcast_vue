aedef9d6fdbc9958c6629f69ffabf6a7dd13efd2
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-02-08 15:38:41 -0500

    fix: AppDivider colour

--- src/components/AppDivider.vue
+++ src/components/AppDivider.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-row class='my-1 ma-0 pa-0' align='center' justify='center' no-gutters>
 		<v-col :cols='`${cols}`' class='ma-0 pa-0'>
-			<v-divider color='primary' />
+			<v-divider  />
 		</v-col>
 	</v-row>
 </template>
